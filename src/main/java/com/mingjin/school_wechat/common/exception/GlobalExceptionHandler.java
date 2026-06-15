c1b661522f55c36458b007212f537eb9698358dc
名尽 <15639987+zxh_cym@user.noreply.gitee.com>
2026-06-15 11:25:39 +0800

    手机端适配

--- src/main/java/com/mingjin/school_wechat/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mingjin/school_wechat/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.mingjin.school_wechat.common.exception;
 
 import com.mingjin.school_wechat.common.api.ApiResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ApiResponse<Void> handleBusinessException(BusinessException ex) {
+    public ApiResponse<Void> handleBusinessException(BusinessException ex, HttpServletResponse response) {
+        response.setContentType("application/json;charset=UTF-8");
         return ApiResponse.fail(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<Void> handleException(Exception ex) {
+    public ApiResponse<Void> handleException(Exception ex, HttpServletResponse response) {
+        response.setContentType("application/json;charset=UTF-8");
         return ApiResponse.fail(ex.getMessage());
     }
 }
